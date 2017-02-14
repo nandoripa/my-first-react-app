ee4ff9e9820d55dbc24bffd6046e74e2b05dbba8
Fernando Rivas Palomo <nandoripa@gmail.com>
2017-02-14 17:38:35 +0100

    Added employee and employee list components

--- jsx/app.js
+++ jsx/app.js
@@ -1,6 +1,42 @@
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      employees: [
+        {id: 1, name: "Mike", email: "mike@foo.dev"},
+        {id: 2, name: "John", email: "john@foo.dev"}
+      ]
+    };
+  }
+
   render () {
-    return (<h1>Mi primera App</h1>);
+    return (
+      <EmployeeList employees={this.state.employees} />
+    );
+  }
+}
+
+class Employee extends React.Component {
+  render() {
+    return (<li>{this.props.name - this.props.email}</li>);
+  }
+}
+
+class EmployeeList extends React.Component {
+  render () {
+    return (
+      <ul>
+        {this.props.employees.map(employee => {
+          return (
+            <Employee
+              key={employee.id}
+              name={employee.name}
+              email={employee.email}
+            />
+          );
+        })}
+      </ul>
+    );
   }
 }
 
