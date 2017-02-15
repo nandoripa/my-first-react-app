5104a8e2bc7667c0716c771bcd279e33a2c09f21
Fernando Rivas Palomo <nandoripa@gmail.com>
2017-02-15 12:46:47 +0100

    Added new employee form

--- jsx/app.js
+++ jsx/app.js
@@ -11,31 +11,57 @@ class App extends React.Component {
 
   render () {
     return (
-      <EmployeeList employees={this.state.employees} />
+      <EmployeeList
+        employees={this.state.employees} 
+        onAddEmployee={this.handleOnAddEmployee.bind(this)}
+      />
     );
+  }
+
+  handleOnAddEmployee (event) {
+    event.preventDefault();
+
+    let newEmployee = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+    };
+
+    this.setState({
+      employees: this.state.employees.concat([newEmployee])
+    })
   }
 }
 
 class Employee extends React.Component {
   render() {
-    return (<li>{this.props.name - this.props.email}</li>);
+    return (<li>{this.props.name} - {this.props.email}</li>);
   }
 }
 
 class EmployeeList extends React.Component {
   render () {
     return (
-      <ul>
-        {this.props.employees.map(employee => {
-          return (
-            <Employee
-              key={employee.id}
-              name={employee.name}
-              email={employee.email}
-            />
-          );
-        })}
-      </ul>
+      <div>
+        <ul>
+          {this.props.employees.map(employee => {
+            return (
+              <Employee
+                key={employee.id}
+                name={employee.name}
+                email={employee.email}
+              />
+            );
+          })}
+        </ul>
+
+        <form onSubmit={this.props.onAddEmployee}>
+          <input id="name" type="text" placeholder="Nombre" name="nombre" />
+          <br/>
+          <input id="email" type="email" placeholder="Email" name="email" />
+          <br/><br/>
+          <input type="submit" value="Guardar" /> 
+      </form>
+     </div>
     );
   }
 }
